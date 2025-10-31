a72d3f81f3d57089fbe4a51e48587e9aec19dffe
zenoyao <2308024406@qq.com>
2025-10-31 09:40:31 +0800

    文件类型调整。

--- net/Scm.Common/Enums/ScmFileEnum.cs
+++ net/Scm.Common/Enums/ScmFileEnum.cs
@@ -5,17 +5,30 @@ namespace Com.Scm.Enums
     public enum ScmFileTypeEnum
     {
         None = 0,
+
         [Description("字节文件")]
-        Byte = 1,
+        Byte = 10,
+
         [Description("字符文件")]
-        Text = 2,
-        [Description("图像文件")]
-        Image = 3,
+        Text = 20,
+        [Description("日志文件")]
+        Logs = 21,
+        [Description("代码文件")]
+        Code = 22,
+
         [Description("媒体文件")]
-        Media = 4,
+        Media = 30,
+        [Description("图像文件")]
+        Image = 31,
+        [Description("音频文件")]
+        Audio = 32,
+        [Description("视频文件")]
+        Vedio = 33,
+
         [Description("办公文件")]
-        Office = 5,
-        [Description("压缩文件")]
-        Archive = 6,
+        Office = 40,
+
+        [Description("归档文件")]
+        Archive = 50,
     }
 }

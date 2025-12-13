754fb1401123325d5abdcd31178ad4a8d5c8b9f7
opemscm <361341288@qq.com>
2025-12-13 08:38:41 +0800

    库存功能。

---
+++ vue/src/views/eam/inv/asset/edit.vue
@@ -0,0 +1,142 @@
+<template>
+	<sc-dialog v-model="visible" show-fullscreen destroy-on-close :title="titleMap[mode]" width="750px" @close="close">
+		<el-form ref="formRef" label-width="100px" :model="formData" :rules="rules">
+			<el-form-item label="SKU" prop="sku_id"> 
+	<el-input 
+		v-model="formData.sku_id" 
+		placeholder="请输入SKU" 
+		:maxlength="20" 
+		show-word-limit 
+		clearable 
+	></el-input> 
+</el-form-item> 
+<el-form-item label="批次数据" prop="bat_id"> 
+	<el-input 
+		v-model="formData.bat_id" 
+		placeholder="请输入批次数据" 
+		:maxlength="20" 
+		show-word-limit 
+		clearable 
+	></el-input> 
+</el-form-item> 
+<el-form-item label="唯一码" prop="codes"> 
+	<el-input 
+		v-model="formData.codes" 
+		placeholder="请输入唯一码" 
+		:maxlength="32" 
+		show-word-limit 
+		clearable 
+	></el-input> 
+</el-form-item> 
+<el-form-item label="使用部门" prop="organize_id"> 
+	<el-input 
+		v-model="formData.organize_id" 
+		placeholder="请输入使用部门" 
+		:maxlength="20" 
+		show-word-limit 
+		clearable 
+	></el-input> 
+</el-form-item> 
+<el-form-item label="使用人员" prop="user_id"> 
+	<el-input 
+		v-model="formData.user_id" 
+		placeholder="请输入使用人员" 
+		:maxlength="20" 
+		show-word-limit 
+		clearable 
+	></el-input> 
+</el-form-item> 
+
+		</el-form>
+
+		<template #footer>
+			<el-button @click="close">取 消</el-button>
+			<el-button :loading="isSaveing" type="primary" @click="save">
+				确 定
+			</el-button>
+		</template>
+	</sc-dialog>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			mode: "add",
+			titleMap: {add: "新增",edit: "编辑"},
+			visible: false,
+			isSaveing: false,
+			formData: this.def_data(),
+			rules: {
+				codec: [
+					{ required: true, trigger: "blur", message: "编码不能为空" },
+					{ required: true, trigger: "blur", message: "编码应4至32个字符", pattern: this.$SCM.REGEX_CODEC },
+				],
+				namec: [
+					{ required: true, trigger: "blur", message: "名称不能为空" },
+					{ required: true, trigger: "blur", message: "名称应4至64个字符", pattern: this.$SCM.REGEX_NAMEC },
+				],
+			},
+		};
+	},
+	mounted() {
+	},
+	methods: {
+		def_data(){
+			return {
+				id: this.$SCM.DEF_ID,
+				sku_id:'', 
+bat_id:'', 
+codes:'', 
+organize_id:'', 
+user_id:'', 
+
+			}
+		},
+		async open(row) {
+			if (!row || !row.id) {
+				this.mode = "add";
+			} else {
+				this.mode = "edit";
+				var res = await this.$API.eaminvasset.edit.get(row.id);
+				this.formData = res.data;
+			}
+			this.visible = true;
+		},
+		save() {
+			this.$refs.formRef.validate(async (valid) => {
+				if (!valid) {
+					return;
+				}
+
+				this.isSaveing = true;
+				let res = null;
+				if (this.$SCM.is_valid_id(this.formData.id)) {
+					res = await this.$API.eaminvasset.update.put(this.formData);
+				} else {
+					res = await this.$API.eaminvasset.add.post(this.formData);
+				}
+				this.isSaveing = false;
+
+				if (res.code == 200) {
+					this.$emit("complete");
+					this.visible = false;
+					this.$message.success("保存成功");
+				} else {
+					this.$alert(res.message, "提示", { type: "error" });
+				}
+			});
+		},
+		close() {
+			this.formData = this.def_data();
+			this.$refs.formRef.resetFields();
+			this.visible = false;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.el-select {
+	width: 100%;
+}
+</style>

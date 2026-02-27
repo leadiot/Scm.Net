46e6a4fc2712407f717b16ec3d9480cbaf201ad7
opemscm <361341288@qq.com>
2026-02-27 09:28:35 +0800

    终端支持二维码。

--- vue/src/views/scm/ur/terminal/info.vue
+++ vue/src/views/scm/ur/terminal/info.vue
@@ -1,19 +1,28 @@
 <template>
-	<sc-dialog v-model="visible" show-fullscreen destroy-on-close :title="title" width="450px" @close="close">
-		<el-form ref="formRef" label-width="100px" :model="formData">
-			<el-form-item label="终端名称" prop="names">
-				<el-input v-model="formData.names" readonly />
-			</el-form-item>
-			<el-form-item label="服务地址" prop="url">
-				<el-input v-model="formData.url" readonly />
-			</el-form-item>
-			<el-form-item label="终端代码" prop="codes">
-				<el-input v-model="formData.codes" readonly />
-			</el-form-item>
-			<el-form-item label="终端口令" prop="pass">
-				<el-input v-model="formData.pass" readonly />
-			</el-form-item>
-		</el-form>
+	<sc-dialog v-model="visible" show-fullscreen destroy-on-close :title="title" width="670px" @close="close">
+		<el-row>
+			<el-col :span="12">
+				<div style="text-align: center;">
+					<scQrcode ref="qrcode" :text="qrCode" :size="200"></scQrcode>
+				</div>
+			</el-col>
+			<el-col :span="12">
+				<el-form ref="formRef" label-width="100px" :model="formData">
+					<el-form-item label="终端名称" prop="names">
+						<el-input v-model="formData.names" readonly />
+					</el-form-item>
+					<el-form-item label="服务地址" prop="url">
+						<el-input v-model="formData.url" readonly />
+					</el-form-item>
+					<el-form-item label="终端代码" prop="codes">
+						<el-input v-model="formData.codes" readonly />
+					</el-form-item>
+					<el-form-item label="终端口令" prop="pass">
+						<el-input v-model="formData.pass" readonly />
+					</el-form-item>
+				</el-form>
+			</el-col>
+		</el-row>
 
 		<template #footer>
 			<el-button type="primary" @click="close">确 定</el-button>
@@ -21,12 +30,17 @@
 	</sc-dialog>
 </template>
 <script>
+import scQrcode from '@/components/scQrcode';
 export default {
+	components: {
+		scQrcode,
+	},
 	data() {
 		return {
 			title: '详情',
 			visible: false,
 			formData: {},
+			qrCode: ''
 		};
 	},
 	mounted() {
@@ -40,6 +54,7 @@ export default {
 				this.formData.url = window.location.host;
 			}
 			this.visible = true;
+			this.qrCode = this.formData.url + '\n' + this.formData.codes + '\n' + this.formData.pass;
 		},
 		close() {
 			this.formData = {};

e3d9e63d1e93999e57ece85bcf8e07579e83f296
opemscm <361341288@qq.com>
2026-03-15 10:19:15 +0800

    添加冲突策略。

---
+++ net/Nas.Common/NasCssEnum.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Com.Scm.Nas
+{
+    /// <summary>
+    /// 冲突策略
+    /// </summary>
+    public enum NasCssEnum
+    {
+        None = 0,
+
+        [Description("保留本地")]
+        KeepNative = 1,
+
+        [Description("保留远端")]
+        KeepRemote = 2,
+
+        [Description("保留所有")]
+        KeepBoth = 3,
+
+        [Description("跳过")]
+        Skip = 4
+    }
+}

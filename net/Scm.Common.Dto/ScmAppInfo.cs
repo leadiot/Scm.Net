ecf4a556271b5a3d45944c9efa9129d8374b6a31
opemscm <361341288@qq.com>
2026-03-23 10:44:16 +0800

    应用信息完善。

--- net/Scm.Common.Dto/ScmAppInfo.cs
+++ net/Scm.Common.Dto/ScmAppInfo.cs
@@ -20,8 +20,28 @@ namespace Com.Scm
         public string name { get; set; }
 
         /// <summary>
+        /// 项目地址
+        /// </summary>
+        public string project { get; set; }
+
+        /// <summary>
+        /// 网站地址
+        /// </summary>
+        public string website { get; set; }
+
+        /// <summary>
         /// 应用简介
         /// </summary>
         public string content { get; set; }
+
+        /// <summary>
+        /// 邮件
+        /// </summary>
+        public string email { get; set; }
+
+        /// <summary>
+        /// 群聊
+        /// </summary>
+        public string qchat { get; set; }
     }
 }

c87929d53ba67eccde0eb6e81aed5b7f0d552ff5
opemscm <361341288@qq.com>
2026-03-24 11:19:45 +0800

    同步周期管理。

---
+++ net/Scm.Common/Enums/ScmCronEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Com.Scm.Enums
+{
+    /// <summary>
+    /// 周期处理
+    /// </summary>
+    public enum ScmCronEnum
+    {
+        None = 0,
+        [Description("实时")]
+        Event = 1,
+        [Description("每隔")]
+        Every = 2,
+        [Description("每到")]
+        Fixed = 3,
+        [Description("满足")]
+        Match = 4,
+        [Description("人工")]
+        Manual = 5,
+    }
+}

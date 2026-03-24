d5ffebed4d2950df49626f826827697652443e7e
opemscm <361341288@qq.com>
2026-03-24 11:55:17 +0800

    添加过滤枚举

---
+++ net/Scm.Common/ScmFilterEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Com.Scm
+{
+    /// <summary>
+    /// 过滤枚举
+    /// </summary>
+    public enum ScmFilterEnum
+    {
+        None = 0,
+        [Description("包含")]
+        Include = 1,
+        [Description("排除")]
+        Exclude = 2,
+    }
+}

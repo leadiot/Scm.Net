f233c50b8ed826cc2a17e98eac2f03d0b1f78a09
opemscm <361341288@qq.com>
2026-04-01 18:26:53 +0800

    10.11.21，大文件同步功能。

--- net/Scm.Api/ScmServerEnv.cs
+++ net/Scm.Api/ScmServerEnv.cs
@@ -10,22 +10,22 @@
         /// <summary>
         /// 次版本号
         /// </summary>
-        public const int MINOR = 10;
+        public const int MINOR = 11;
 
         /// <summary>
         /// 修订号
         /// </summary>
-        public const int PATCH = 20;
+        public const int PATCH = 21;
 
         /// <summary>
         /// 构建号
         /// </summary>
-        public const string BUILD = "2026033001";
+        public const string BUILD = "2026040201";
 
         /// <summary>
         /// 发行日期
         /// </summary>
-        public const string RELEASE_DATE = "2026-03-30";
+        public const string RELEASE_DATE = "2026-04-02";
 
         public static readonly string VERSION = $"{MAJOR}.{MINOR}.{PATCH}";
     }

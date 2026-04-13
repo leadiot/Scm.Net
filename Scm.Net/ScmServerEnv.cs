06319ffb7517820707dd62dec4e915318bd7aa4a
opemscm <361341288@qq.com>
2026-04-13 16:39:31 +0800

    10.12.24版本。

--- Scm.Net/ScmServerEnv.cs
+++ Scm.Net/ScmServerEnv.cs
@@ -15,17 +15,17 @@
         /// <summary>
         /// 修订号
         /// </summary>
-        public const int PATCH = 23;
+        public const int PATCH = 24;
 
         /// <summary>
         /// 构建号
         /// </summary>
-        public const string BUILD = "2026041201";
+        public const string BUILD = "2026041301";
 
         /// <summary>
         /// 发行日期
         /// </summary>
-        public const string RELEASE_DATE = "2026-04-12";
+        public const string RELEASE_DATE = "2026-04-13";
 
         public static readonly string VERSION = $"{MAJOR}.{MINOR}.{PATCH}";
     }

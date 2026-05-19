9affc7f15d3bf7d5975265032e156c4e6a943167
leadiot <361341288@qq.com>
2026-05-19 17:05:18 +0800

    添加文件和目录功能。

---
+++ Scm.Core/Sys/Files/Dvo/AddFileRequest.cs
@@ -0,0 +1,11 @@
+﻿using Com.Scm.Request;
+
+namespace Com.Scm.Sys.Files.Dvo
+{
+    public class AddFileRequest : ScmRequest
+    {
+        public string path { get; set; }
+
+        public string name { get; set; }
+    }
+}

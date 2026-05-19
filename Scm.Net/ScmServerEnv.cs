06a295dad91a19e5a80292e0ad9c53e8cf8bbc35
leadiot <361341288@qq.com>
2026-05-19 11:24:16 +0800

    10.14.32版本。

--- Scm.Net/ScmServerEnv.cs
+++ Scm.Net/ScmServerEnv.cs
@@ -6,25 +6,25 @@
         /// 发行日期
         /// TODO: （必需修改）发行版本，发布前需要修改此处
         /// </summary>
-        public const string VER_DATE = "2026-05-12";
+        public const string VER_DATE = "2026-05-20";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 YYYYMMDDXX，其中 XX 是当天的第几次构建
         /// </summary>
-        public const string VER_CODE = "2026051201";
+        public const string VER_CODE = "2026052001";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示构建版本号，通常在每次构建时增加，即使没有功能更新或错误修复
         /// </summary>
-        public const int BUILD = 53;
+        public const int BUILD = 54;
 
         /// <summary>
         /// 修正版本
         /// TODO: （可选修改）修正版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示修正版本号，通常在有错误修复但没有新功能添加时增加
         /// </summary>
-        public const int PATCH = 31;
+        public const int PATCH = 32;
 
         /// <summary>
         /// 次要版本

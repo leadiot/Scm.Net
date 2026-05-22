fbd8ea5c7330776ec40fe1466e4468599a61d71f
leadiot <361341288@qq.com>
2026-05-22 14:15:30 +0800

    MQTT示例完善。

--- Samples.Server.Dao/Mqtt/TemperatureDataDao.cs
+++ Samples.Server.Dao/Mqtt/TemperatureDataDao.cs
@@ -1,15 +1,18 @@
 using Com.Scm.Dao;
+using SqlSugar;
 
 namespace Com.Scm.Samples.Mqtt
 {
     /// <summary>
     /// 温度数据 DAO
     /// </summary>
+    [SugarTable("samples_temperature_data")]
     public class TemperatureDataDao : ScmDao
     {
         /// <summary>
         /// 设备 ID
         /// </summary>
+        [SugarColumn(Length = 16)]
         public string device_id { get; set; }
 
         /// <summary>
@@ -18,7 +21,7 @@ namespace Com.Scm.Samples.Mqtt
         public float temperature { get; set; }
 
         /// <summary>
-        /// 记录时间
+        /// 记录时间（Unix 毫秒戳）
         /// </summary>
         public long timestamp { get; set; }
     }

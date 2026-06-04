e9030066e291ef2e1d91a23fe39f9179f0d898f5
avenyao <361341288@qq.com>
2026-06-04 14:04:17 +0800

    10.15.34.57版本

--- Scm.Net/ScmServerEnv.cs
+++ Scm.Net/ScmServerEnv.cs
@@ -6,19 +6,19 @@
         /// 发行日期
         /// TODO: （必需修改）发行版本，发布前需要修改此处
         /// </summary>
-        public const string VER_DATE = "2026-05-29";
+        public const string VER_DATE = "2026-06-04";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 YYYYMMDDXX，其中 XX 是当天的第几次构建
         /// </summary>
-        public const string VER_CODE = "2026052901";
+        public const string VER_CODE = "2026060401";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示构建版本号，通常在每次构建时增加，即使没有功能更新或错误修复
         /// </summary>
-        public const int BUILD = 56;
+        public const int BUILD = 57;
 
         /// <summary>
         /// 修正版本

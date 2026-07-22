1d0f9fcef2dec90e58abb3a7b3f2bd24b1a674a7
avenyao <361341288@qq.com>
2026-07-22 14:01:44 +0800

    10.19.39.65

--- Scm.Net/ScmServerEnv.cs
+++ Scm.Net/ScmServerEnv.cs
@@ -6,31 +6,31 @@
         /// 发行日期
         /// TODO: （必需修改）发行版本，发布前需要修改此处
         /// </summary>
-        public const string VER_DATE = "2026-07-14";
+        public const string VER_DATE = "2026-07-22";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 YYYYMMDDXX，其中 XX 是当天的第几次构建
         /// </summary>
-        public const string VER_CODE = "2026071401";
+        public const string VER_CODE = "2026072201";
 
         /// <summary>
         /// 构建版本
         /// TODO: （必需修改）构建版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示构建版本号，通常在每次构建时增加，即使没有功能更新或错误修复
         /// </summary>
-        public const int BUILD = 64;
+        public const int BUILD = 65;
 
         /// <summary>
         /// 修正版本
         /// TODO: （可选修改）修正版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示修正版本号，通常在有错误修复但没有新功能添加时增加
         /// </summary>
-        public const int PATCH = 38;
+        public const int PATCH = 39;
 
         /// <summary>
         /// 次要版本
         /// TODO: （可选修改）次要版本，发布前需要修改此处，格式为 X，其中 X 是一个整数，表示次要版本号，通常在有新功能添加但保持向后兼容时增加
         /// </summary>
-        public const int MINOR = 18;
+        public const int MINOR = 19;
 
         /// <summary>
         /// 主要版本

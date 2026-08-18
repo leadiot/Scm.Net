22c60925e8a48980fe7c2085f98216faf2ac2ecc
avenyao <361341288@qq.com>
2026-08-18 12:47:04 +0800

    数据结构完善。

--- Scm.Dao/Sys/Sms/ScmSysSmsTerminalDao.cs
+++ Scm.Dao/Sys/Sms/ScmSysSmsTerminalDao.cs
@@ -22,6 +22,11 @@ namespace Com.Scm.Nas.App
         public long terminal_id { get; set; }
 
         /// <summary>
+        /// 修改时间
+        /// </summary>
+        public long modify_time { get; set; }
+
+        /// <summary>
         /// 其它附加参数
         /// </summary>
         [SugarColumn(Length = 1024, IsNullable = true, IsJson = true)]
